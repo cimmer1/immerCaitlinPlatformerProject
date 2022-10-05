001484fc8dc410fbdbdb520ab8c5a8293079eb25
cimmer1 <cimmer@mail.bradley.edu>
2022-10-05 14:31:56 -0500

    messing with movement

--- Assets/Scripts/RaindropBehaviour.cs
+++ Assets/Scripts/RaindropBehaviour.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class RaindropBehaviour : MonoBehaviour
 {
-    public float Speed;
+    public float speed;
 
+    public float jumpForce;
+    
+    bool touchingGround = true;
+
+   
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if (touchingGround && Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;      
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             float xMove = Input.GetAxis("Horizontal");
             Vector2 newPos = transform.position;
 
-            newPos.x += xMove * Speed * Time.deltaTime;
+            newPos.x += xMove * speed * Time.deltaTime;
 
             Debug.Log(newPos);
             transform.position = newPos;
@@ -31,10 +45,20 @@ public class RaindropBehaviour : MonoBehaviour
             float xMove = Input.GetAxis("Horizontal");
             Vector2 newPos = transform.position;
 
-            newPos.x += xMove * Speed * Time.deltaTime;
+            newPos.x += xMove * speed * Time.deltaTime;
 
             Debug.Log(newPos);
             transform.position = newPos;
         }
+
     }
+
+    private void OnCollision2D(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            touchingGround = false;
+        }
+    }
+
 }
